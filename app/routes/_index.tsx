ed84451ea1d0e0a0e4e4a681da99d23b6963a966
Your Name <you@example.com>
2025-02-21 16:48:50 +0100

    uncommenting lucide-react to see if its causing deployment issues

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,7 +13,7 @@ export default function Index() {
   return (
     <div className=" flex gap-4">
       Job Board
-      <Link to={'/job'}>Go to listings</Link>
+      <Button><Link to={'/job'}>Go to listings</Link></Button>
     </div>
   );
 }
