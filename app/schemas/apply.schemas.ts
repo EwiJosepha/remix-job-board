32c60cb75beb9ab169ccd4abdbe6bf42f6decbfe
Your Name <you@example.com>
2025-03-03 16:29:32 +0100

    dashboard layout with shadcn

--- app/schemas/apply.schemas.ts
+++ app/schemas/apply.schemas.ts
@@ -2,7 +2,7 @@ import z from 'zod'
 export const applySchema = z.object({
   title: z.string(),
   company: z.string(),
-  email: z.string().email(),
-  firstName: z.string(),
-  resume: z.string()
+  email: z.string().email({message: 'invalid email address'}),
+  firstName: z.string({message: 'First name is required'}).min(2, {message: 'first name too short'}).max(50, {message: 'first name too long'}),
+  resume: z.string({message: 'resume value is empty or invalid format'})
 })
